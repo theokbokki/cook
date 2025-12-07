6bb0192bc22daea3f7cd24830dd4c02129eb0bea
théoo <90846584+theokbokki@users.noreply.github.com>
2025-12-07 20:57:24 +0100

    Add some sidebar styling

--- resources/views/components/⚡sidebar.blade.php
+++ resources/views/components/⚡sidebar.blade.php
@@ -12,15 +12,23 @@ new class extends Component
     {
         $this->recipes = Recipe::all();
     }
+
+    public function selected($recipe) {
+        return request()->is('recipes/'.$recipe->slug);
+    }
 };
 ?>
 
-<div>
-    <ul>
-    @foreach($recipes as $recipe)
-        <li>
-            <a href="{{ route('recipes.show', $recipe) }}" wire:navigate>{{ $recipe->name }}</a>
-        </li>
-    @endforeach
-    </ul>
-</div>
+<nav class="sidebar">
+    <h2 class="sro">Main navigation</h2>
+    <div class="sidebar__section">
+        <p class="sidebar__subtitle">Recipes</p>
+        <ul class="sidebar__list">
+        @foreach($recipes as $recipe)
+            <li>
+                <a href="{{ route('recipes.show', $recipe) }}" wire:navigate class="sidebar__link" data-selected="{{ $this->selected($recipe) }}">{{ $recipe->name }}</a>
+            </li>
+        @endforeach
+        </ul>
+    </div>
+</nav>
